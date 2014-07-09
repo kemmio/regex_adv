ed2700df977bc115237f9eef5cafb683f24a062f
unknown <kemmio60@gmail.com>
2014-07-10 02:56:16 +0400

    html validate

---
+++ regex_adv.js
@@ -0,0 +1,8 @@
+function validateHTML(html)
+{
+    return html.match(/[^]*<\w+\s+[^]*id\s*=\s*(("(\w+)")|(\w+))(>|\s+[^]*>)[^]*(?=<\w+\s+[^]*id\s*=\s*(("\1"|\1)(>|\s+[^]*>)))/m)==null;
+}
+
+console.log(validateHTML("<html><span id=1>\n<span id=1><\html>"));
+
+console.log(validateHTML("<html><span id=1>\n<span id=2><\html>"));
